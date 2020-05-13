b7efd19eaf897700184a0a6fd8ed5db51d0128dc
Chris Townsend <christopher.townsend@canonical.com>
2020-05-13 12:03:26 -0400

    [platform] Add selection of VMImageVault depending on driver
    The appropriate VM image vault will be chosen depending on if "lxd" or any other
    driver is set.

--- src/daemon/daemon_config.cpp
+++ src/daemon/daemon_config.cpp
@@ -131,7 +131,8 @@ std::unique_ptr<const mp::DaemonConfig> mp::DaemonConfigBuilder::build()
         {
             hosts.push_back(image.get());
         }
-        vault = std::make_unique<DefaultVMImageVault>(
+
+        vault = platform::make_image_vault(
             hosts, url_downloader.get(),
             mp::utils::backend_directory_path(cache_directory, factory->get_backend_directory_name()),
             mp::utils::backend_directory_path(data_directory, factory->get_backend_directory_name()), days_to_expire);
